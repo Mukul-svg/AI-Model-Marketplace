ce9b382a85efecb580280c32284b28c7e06c42a7
Mukul Raghav <mukulraghav2003@gmail.com>
2024-11-07 12:57:56 +0530

    add

---
+++ client/src/app/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from "./Footer.module.css";
+import Image from "next/image";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <></>
+  );
+}
